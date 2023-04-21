541e22cc740de551a5c33345425e3d6eb0105618
khiziresmars <113808648+khiziresmars@users.noreply.github.com>
2023-04-22 05:48:07 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "NEKit",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_11)
+    ],
+    products: [
+        .library(
+            name: "NEKit",
+            targets: ["NEKit"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/zhuhaow/YamlSwift.git", from: "3.4.4"),
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket.git", from: "7.6.5"),
+        .package(url: "https://github.com/zhuhaow/Resolver.git", from: "0.3.1"),
+    ],
+    targets: [
+        .target(
+            name: "NEKit",
+            dependencies: [
+                .product(name: "Yaml", package: "YamlSwift"),
+                .product(name: "CocoaAsyncSocket", package: "CocoaAsyncSocket"),
+                .product(name: "Resolver", package: "Resolver"),
+            ],
+            path: "src"
+        ),
+    ]
+)
